de7e07fa1ae91e5981aec137ad2e1a2f25287dc1
nus3 <hadayota33@gmail.com>
2021-11-07 14:47:21 +0900

    :green_heart: Add deploy script and ci

---
+++ scripts/deploy-gist/index.ts
@@ -0,0 +1,31 @@
+import { Octokit } from 'octokit'
+
+import words from '../../words.json'
+
+const GIST_ID = '8107d42ebf5205864f8825774f2acd95'
+const GIST_NAME = 'words.json'
+
+const { GITHUB_TOKEN } = process.env
+
+const main = async () => {
+  try {
+    const client = new Octokit({
+      auth: GITHUB_TOKEN,
+    })
+    await client.rest.gists.update({
+      gist_id: GIST_ID,
+      headers: { accept: 'application/vnd.github.v3+json' },
+      files: {
+        [`${GIST_NAME}`]: {
+          content: JSON.stringify(words),
+        },
+      },
+    })
+
+    console.info('Completed update words')
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+main()
